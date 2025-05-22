a26de45409d26b89f6ffb57053625ecf35c74f37
TayebaN <tayebe.naseri.tn@gmail.com>
2025-05-22 11:19:43 +0200

    Added new title

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/listina-loga.svg'
-import viteLogo from './assets/blueshappe.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/listina-loga.svg";
+import viteLogo from "./assets/blueshappe.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        
-        
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Examensarbete💪✨🎉</h1>
+      <h1>Examensarbete Tillsammans💪✨🎉</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          litte till
-        </p>
+        <p>litte till</p>
       </div>
-      <p>
-        litte täxt och sådant
-      </p>
+      <p>litte täxt och sådant</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
